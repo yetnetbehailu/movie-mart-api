8c09216e105431749307579236838a533ffb75ce
yeti <yetnet.behailu@hotmail.com>
2024-04-24 19:30:51 +0200

    Implemented user login functionality, JWT token generation & authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -68,8 +68,62 @@ namespace movie_mart_api.Controllers
             }
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User model)
+        {
+            // Find user based on username
+            var user = await _userManager.FindByNameAsync(model.UserName);
 
+            // If username not found look for user by email
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(model.Email);
+            }
+
+            // If user found & password correct
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.PasswordHash))
+            {
+                // Convert user Id to string
+                string userIdString = user.Id.ToString();
+
+                // Create JWT claims with user ID and username
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userIdString),
+                    new Claim(ClaimTypes.Name, user.UserName)
+                };
+
+                // Create key & credentials to generate JWT token
+                var jwtKey = _configuration["Jwt:Key"]; //config in appsettings.json
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)); // Class used to sign the JWT token created using bytes from config setting "Jwt:Key"
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256); // Signing credentials class, requires security key & an algorithm(HMAC (Hash-based Message Authentication Code))
+
+                /* Create JWT token
+                   JwtSecurityToken class represents the JWT token itself
+                   issuer: The identity provider or authentication server
+                   audience: Receiver of token, the client or application that will consume the token
+                   claims: Statements about the user (e.g., user ID, username).
+                   expires: Expiration date token considered invalid. Renew by signing in again 
+                   signingCredentials: Used to sign the token, includes the security key & algorithm
+                */
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: _configuration["Jwt:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(30),
+                    signingCredentials: creds
+                );
+
+                // Return OK with token converted to a string
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+
+            // Return 401 Unauthorized if login fails
+            return Unauthorized("Invalid username, email or password");
+
+        }
 
     }
+
 }
 
